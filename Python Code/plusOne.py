b33ca57366916ac845e2561f9e8c6d27eaa4f1f0
shreyakate <shreya.kate@gmail.com>
2018-04-07 15:39:48 -0400

    plus one optimized

--- Python Code/plusOne.py
+++ Python Code/plusOne.py
@@ -1,12 +1,12 @@
 def plusOne(digits):
 	carry = 1
 	i = len(digits)-1
-	while i >=0:
+	while i >=0 and carry:
 		digits[i], carry = (digits[i]+carry)%10  , (digits[i]+carry)/10
 		i -= 1
-	if carry>0:
+	if carry:
 		digits.insert(0,carry)
 	return digits
 
-
 print plusOne([9,9,9])
+print plusOne([1,0,0])
