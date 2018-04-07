880b7970f0f9d60b7af4e014e2a0e6c40bac0383
shreyakate <shreya.kate@gmail.com>
2018-04-07 15:13:49 -0400

    Number to String/ English Words

---
+++ Python Code/IntegerToWords.py
@@ -0,0 +1,57 @@
+def IntegerToWords(num):
+	if num == 0: 
+		return 'Zero'
+
+	digits =['','One ','Two ','Three ','Four ','Five ','Six ','Seven ','Eight ', 'Nine ']
+	teens = ['Ten ', 'Eleven ', 'Twelve ', 'Thirteen ', 'Fourteen ', 'Fifteen ', 'Sixteen ', 'Seventeen ', 'Eighteen ','Nineteen ']
+	tens = ['', '', 'Twenty ', 'Thirty ', 'Forty ', 'Fifty ', 'Sixty ', 'Seventy ', 'Eighty ', 'Ninety ']
+	others = ['', 'Thousand ', 'Million ', 'Billion ']
+	
+	res =''
+	x = str(num)
+	if len(x)%3 == 1: x ='00'+ x
+	elif len(x)%3 == 2: x = '0'+ x
+	nums = [x[i:i+3] for i in range(0,len(x),3)][::-1]
+	for i in range(len(nums)):
+		if nums[i].count('0')==3:
+			continue
+		res = others[i] + res
+		if nums[i][1]=='1':
+			res = teens[int(nums[i][2])] + res
+		else:
+			res = digits[int(nums[i][2])] + res
+			res = tens[int(nums[i][1])] + res
+		if nums[i][0]>'0':
+			res = digits[int(nums[i][0])] + 'Hundred '+ res
+	return res.rstrip()
+
+def numberToWords(num):
+	if num == 0: 
+		return 'Zero'
+
+	digits =['','One ','Two ','Three ','Four ','Five ','Six ','Seven ','Eight ', 'Nine ']
+	teens = ['Ten ', 'Eleven ', 'Twelve ', 'Thirteen ', 'Fourteen ', 'Fifteen ', 'Sixteen ', 'Seventeen ', 'Eighteen ','Nineteen ']
+	tens = ['', '', 'Twenty ', 'Thirty ', 'Forty ', 'Fifty ', 'Sixty ', 'Seventy ', 'Eighty ', 'Ninety ']
+	others = ['', 'Thousand ', 'Million ', 'Billion ']
+
+	
+	def helper(num):
+		if num == 0 : return ''
+		elif num<10: return digits[num]
+		elif 9<num<20: return teens[num-10]
+		elif num < 100:
+			return tens[num/10] + helper(num%10)
+		else:
+			return digits[num/100] + 'Hundred '+ helper(num%100)
+
+	res = ''
+	i = 0
+	while num > 0:
+		if num%1000 !=0:
+			res = helper(num%1000) + others[i] + res
+		num /= 1000
+		i += 1
+	return res.strip()
+
+print IntegerToWords(12345619)
+print numberToWords(12345619)
--- Python Code/detectCycleUndirected.py
+++ Python Code/detectCycleUndirected.py
@@ -19,6 +19,7 @@ class Graph:
 				return True
 
 		return False
+		
 	def detectCycleUndirected(self):
 		visited = [False] * self.vertices
 
