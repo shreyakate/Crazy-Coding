aace7f7a4223d91094ee3b09969ab30a23f2e117
shreyakate <shreya.kate@gmail.com>
2018-04-10 21:46:10 -0400

    parenthesis

--- Python Code/ValidParenthesis.py
+++ Python Code/ValidParenthesis.py
@@ -1,3 +1,23 @@
 def ValidParenthesis(s):
-	l, r= 0,0
+	if len(s)%2 != 0: return False
+	stack =[]
+	for p in s:
+		if p == '(' or p == '{' or p =='[':
+			stack.append(p)
+		elif p == ')':
+			if stack :
+				x =stack.pop()
+				if x != '(': return False
+		elif p == '}':
+			if stack :
+				x =stack.pop()
+				if x != '{': return False
+		elif p == ']':
+			if stack :
+				x =stack.pop()
+				if x != '[': return False
 	
+	return len(stack)==0
+
+
+print ValidParenthesis('()')
