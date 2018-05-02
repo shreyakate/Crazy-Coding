908c42a683a4f665a4935fa350bcdb516b0e2935
shreyakate <shreya.kate@gmail.com>
2018-05-01 22:14:56 -0400

    Stocks problem

--- Python Code/BuySellStock2.py
+++ Python Code/BuySellStock2.py
@@ -11,6 +11,26 @@ def BuySellStock2(prices, fee):
 				buy = p - fee
 	return profit 
 	
+	buy = prices[0]
+        tmp1, tmp2 = 0,0
+        n = len(prices)
+        for i in range(1,n):
+            if prices[i] < buy:
+                buy = prices[i]
+            else:
+                sell = prices[i]
+                prof = max(sell - buy,0)
+                if prof == tmp1:
+                    tmp1 = tmp2 = prof
+                elif prof > tmp1:
+                    tmp2 = tmp1
+                    tmp1 = prof
+                elif prof > tmp2:
+                    tmp2 = prof
+                prof = 0
+                buy = prices[i]
+        
+        return tmp1+tmp2
 
 
 print BuySellStock2([7,1,3,5,9,1,3],2)
